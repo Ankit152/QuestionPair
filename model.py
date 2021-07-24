1170caab43c821727fd81509442c2f621bb4c21b
Ankit Kurmi <ankitkurmi152@gmail.com>
2021-07-25 01:54:54 +0530

    added changes to the model

--- model.py
+++ model.py
@@ -3,26 +3,28 @@ from tensorflow.keras.layers import Bidirectional, LSTM, Dropout
 from tensorflow.keras.layers import Embedding, Dense, Flatten
 from tensorflow.keras.models import Model,Sequential
 from tensorflow.keras.layers import BatchNormalization,Multiply
+from tensorflow.keras.models import Sequential, Model
 
-def helper(input_dim,output_dim,input_length):
+def helper(input_dim,output_dim,input_length,weights):
     model = Sequential()
-    model.add(Embedding(input_dim=input_dim,output_dim=output_dim,input_length=input_length))
+    model.add(Embedding(input_dim=input_dim,output_dim=output_dim,input_length=input_length,weights=[weights],trainable=False))
     model.add(Bidirectional(LSTM(128, activation = 'tanh', return_sequences = True)))
     model.add(Dropout(0.2))
     model.add(Bidirectional(LSTM(128, activation = 'relu', return_sequences = True)))
     model.add(Dropout(0.2))
     model.add(Bidirectional(LSTM(128, activation = 'tanh')))
     model.add(Dense(64, activation = 'relu'))
-    model.add(Dense(16, activation='tanh'))
+    model.add(Dense(32, activation='tanh'))
     return model
 
 
-def model(input_dim,output_dim,input_length):
-    mq1 = helper(2000,128,30)
-    mq2 = helper(2000,128,30)
-    model = Multiply()([mq1.output,mq2.output])
-    model = Flatten()(model)
-    print(model.shape)
-
-model()
+def model(input_dim,output_dim = 100,input_length = 100,weights):
+    mq1 = helper(input_dim,output_dim,input_length,weights)
+    mq2 = helper(input_dim,output_dim,input_length,weights)
+    x = Multiply()([mq1.output,mq2.output])
+    x = Flatten()(x)
+    x = Dense(32,activation="relu")(x)
+    x = Dense(2,activation="softmax")(x)
+    model = Model(inputs = [mq1.input,mq2.input], outputs = x)
+    return model
 
